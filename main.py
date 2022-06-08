c72f1d32a0ab982ddc7a5a8605f5286a9d61428e
maxoman999 <maksimn11022003@gmail.com>
2022-06-09 00:07:37 +0300

    upd1

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ vk = vk_api.VkApi(token=token)
 session_api = vk.get_api()
 longpoll = VkLongPoll(vk)
 
+
 def searh(text):
     places = ['7 шк', 'пожарк', 'таремско', 'лини', 'юж', 'редгум', 'кочк', 'башн', 'ред гум', 'шашлычк',
               'заправк', 'ждан', 'редгум', 'фок', 'мерид', 'молявино', 'автосуш', 'авто суш', 'реан',
               'низ', 'базар', 'централ', 'тумботино', 'налог', 'линии', '66 гараж']
-    for i in range(len(places)):
-        if places[i].lower() in text:
-            return True
+    actions = ['стоят', 'палк', 'веста', 'стоит', 'работа', 'встали']
+    fl1, fl2 = False, False
+    for place in places:
+        if place in text:
+            fl1 = True
+            break
+    for action in actions:
+        if action in text:
+            fl2 = True
+            break
+    if fl1 and fl2:
+        return True
 
 
 def send_message(user_id, message):
@@ -25,9 +35,13 @@ def send_message(user_id, message):
 # send_message('234776693', 'ghgh')
 
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.user_id == 240:
+    if event.type == VkEventType.MESSAGE_NEW and event.user_id == 213813749:
         if searh(event.text):
+            print(event.text)
             send_msg(234776693, event.text)
+        else:
+            print(1)
+    else:print(2)
 
 
 
